d3a214e6acdca8f270855c63ec32876d57774569
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-07-01 21:57:59 +0900

    ADD : Spring Boot 프로젝트 추가

---
+++ Spring_Boot_Practice/src/main/java/kr/hs/dgsw/hyeonbin/Spring_Boot_Practice/SpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.hyeonbin.Spring_Boot_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot_Practice/src/test/java/kr/hs/dgsw/hyeonbin/Spring_Boot_Practice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.hyeonbin.Spring_Boot_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
