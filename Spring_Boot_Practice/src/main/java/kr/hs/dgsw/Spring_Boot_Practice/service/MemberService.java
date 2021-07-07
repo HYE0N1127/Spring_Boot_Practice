81483bd17ac1356afed3823fbb95517855c108f0
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-07-07 15:46:27 +0900

    ADD : 회원 서비스 개발

---
+++ Spring_Boot_Practice/src/main/java/kr/hs/dgsw/Spring_Boot_Practice/service/MemberService.java
@@ -0,0 +1,44 @@
+package kr.hs.dgsw.Spring_Boot_Practice.service;
+
+import kr.hs.dgsw.Spring_Boot_Practice.domain.Member;
+import kr.hs.dgsw.Spring_Boot_Practice.repository.MemberRepository;
+import kr.hs.dgsw.Spring_Boot_Practice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // 회원가입
+        // 같은 이름의 중복 회원 X
+
+        validateDuplicateMember(member);        // 중복 회원 검증
+
+
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())       //반환 형이 Optional 임
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+
+        // ifPresent 메서드 : 이미 값이 있는지를 체크해줌
+        // Optional 자료형이기에 가능함
+    }
+
+    // 전체 회원 조회회
+   public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
