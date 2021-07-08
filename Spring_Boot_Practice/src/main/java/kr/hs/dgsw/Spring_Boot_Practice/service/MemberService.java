a38f7f579346312e6a3f6d8b0b768e7dd6220e3a
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-07-08 21:00:21 +0900

    ADD : 회원 서비스 테스트

--- Spring_Boot_Practice/src/main/java/kr/hs/dgsw/Spring_Boot_Practice/service/MemberService.java
+++ Spring_Boot_Practice/src/main/java/kr/hs/dgsw/Spring_Boot_Practice/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         // 회원가입
@@ -37,7 +41,7 @@ public class MemberService {
    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
-    
+
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
